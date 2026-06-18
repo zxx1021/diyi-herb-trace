bd3db834f485e646a7b5a55f5bb59a188aed2fa0
zxx1021 <2875106742@qq.com>
2026-06-18 21:36:20 +0800

    Update client/src/App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <Layout>
+  <div v-if="!ready" class="app-loader"></div>
+  <Layout v-else-if="useLayout">
     <router-view />
   </Layout>
+  <router-view v-else />
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import Layout from './components/Layout.vue'
+
+const route = useRoute()
+const router = useRouter()
+const ready = ref(false)
+
+router.isReady().then(() => { ready.value = true })
+
+const standalonePages = ['traceability', 'batch-entry', 'batch-env', 'farmer-entry']
+const useLayout = computed(() => !standalonePages.includes(route.name as string))
 </script>
+
+<style>
+.app-loader { min-height: 100vh; background: var(--paper); }
+</style>
